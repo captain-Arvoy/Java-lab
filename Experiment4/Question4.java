0872091dd8bdf121f3a07137b32c643131eb9776
captain-arvoy <accofstardust@gmail.com>
2023-08-30 23:36:57 +0530

    updated question 4

--- Experiment4/Question4.java
+++ Experiment4/Question4.java
@@ -1,18 +1,17 @@
 public class Question4 {
   public static void main(String[] args) {
-    fiboonaci(new Fibo().series(5));
+    new Fibo().series(5);
   }
 }
 class Fibo{
   void series(int n){
-    int a = 0, b = 1,sum = a+b;
-    System.out.println(a+" ");
-    for (int i = 0; i < n; i++) {
-      a = b;
-      sum = a + b;
-      System.out.print(sum+" ");
+    int a = 0, b = 1, sum = a+b;
+    System.out.print(a+" ");
+    for (int i = 0; i < n - 1; i++) {
       b = sum;
+      System.out.print(sum+" ");
+      sum = a + b;
+      a = b;
     }
-   }
   }
 }
